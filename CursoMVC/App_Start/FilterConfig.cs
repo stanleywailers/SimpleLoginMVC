616adf45b987720572214851232b330da3eeff5a
ricardo melara <ricardo.melara@novatechdev.com>
2021-05-16 21:10:01 -0600

    consume user list and show in view

--- CursoMVC/App_Start/FilterConfig.cs
+++ CursoMVC/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace CursoMVC
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filters.VerifySession());
         }
     }
 }
